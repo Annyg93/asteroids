85befd50856166b6b6c975236139d4fda2614008
Annyg93 <andrenygaard@gmail.com>
2025-01-20 19:43:56 +0100

    Player turning

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def main():
 
         pygame.Surface.fill(screen, (0,0,0))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
