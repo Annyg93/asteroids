150df56122173c4cd3196d5623502a5c9c48258e
Annyg93 <andrenygaard@gmail.com>
2025-01-20 22:43:53 +0100

    final of the day

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y, PLAYER_RADIUS)
+
     
     print(f"Starting asteroids!\n Screen width: {SCREEN_WIDTH} \n Screen height: {SCREEN_HEIGHT}")
     
@@ -20,9 +25,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+
         pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
